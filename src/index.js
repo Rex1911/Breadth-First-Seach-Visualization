baac97c17254662972b611e6ead2d2ef2344d3bf
Aryan Likhite <aryanlikhite@gmail.com>
2019-09-12 16:36:12 +0530

    Running on commit 826ac3b from master

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import p5 from 'p5/lib/p5.min';
-import s, {startSearch,clearWalls,clearPath, createMazeHelper} from './app/sketch';
-
-document.querySelector("#start").onclick = startSearch;
-document.querySelector("#clearWalls").onclick = clearWalls;
-document.querySelector("#clearPath").onclick = clearPath;
-document.querySelector("#createMaze").onclick = createMazeHelper;
-
-new p5(s)
